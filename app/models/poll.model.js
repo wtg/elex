7c04f56b25c6ecf3ab786eb0c5f18269851b8a67
sensenChen <chens16@rpi.edu>
2016-04-30 15:40:54 -0400

    fixing conflict?

---
+++ app/models/poll.model.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+// Vote DB Model
+// module.exports allows us to pass this to other files when it is called
+module.exports = mongoose.model('Poll', {
+  result      : String,
+  meeting     : Number,
+  user        : Number,
+  name        : String,
+  Description : String,
+  ID          : mongoose.Schema.ObjectId
+});
