c45aa67196b2c863042ab72dfc13a39fbecfec29
Tristan Villamil <villamil101@gmail.com>
2016-05-01 15:12:31 -0400

    Pin half-fixes
    I'm working on it

--- app/routes/poll.routes.js
+++ app/routes/poll.routes.js
@@ -190,6 +190,12 @@ module.exports = function (app, cas, server) {
             });
         });
 
+		socket.on('meeting', function (meet){
+			Meeting.findOne({_id : meet}, function (m){
+				socket.emit('group', m.group);
+			});
+		});
+
         socket.on('disconnect', function () {
             Participant.remove({ clientSocketID: socket.id }).then(function (data) {
                 console.log("Socket " + socket.id + " successfully disconnected.");
